f428e1ca717a0e128d9a6eaa35446def8d897257
Edvan Junior <edvanbrito.j@hotmail.com>
2024-02-24 17:04:12 -0300

    fix: adjust controller for validade writingScore value

--- src/main/java/com/example/cloudhuman/controller/ProController.java
+++ src/main/java/com/example/cloudhuman/controller/ProController.java
@@ -1,7 +1,9 @@
 package com.example.cloudhuman.controller;
 
+import com.example.cloudhuman.exceptions.InvalidWritingScoreException;
 import com.example.cloudhuman.models.Pro;
 import com.example.cloudhuman.services.ProService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +21,18 @@ public class ProController {
 
     @PostMapping(value = "/validate")
     public ResponseEntity<String> evaluate(@RequestBody String json) throws Exception {
-        Pro pro =  mapper.readValue(json, Pro.class);
-        String response = service.assignProjectByScore(pro);
-        return ResponseEntity.ok(response);
+        Pro pro = mapper.readValue(json, Pro.class);
+        try {
+            float writingScore = pro.getWritingScore();
+            if (writingScore < 0 || writingScore > 1) {
+                throw new InvalidWritingScoreException("writing_score must be between 0 and 1");
+            }
+
+            String response = service.assignProjectByScore(pro);
+            return ResponseEntity.ok(response);
+
+        } catch (InvalidWritingScoreException | JsonProcessingException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/example/cloudhuman/exceptions/InvalidWritingScoreException.java
@@ -0,0 +1,8 @@
+package com.example.cloudhuman.exceptions;
+
+public class InvalidWritingScoreException extends RuntimeException{
+
+    public InvalidWritingScoreException(String message) {
+        super(message);
+    }
+}
