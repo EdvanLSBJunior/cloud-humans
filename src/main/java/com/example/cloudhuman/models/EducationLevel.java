de5508568d3dc04c9fea6222e2feef1eb303dbc6
Edvan Junior <edvanbrito.j@hotmail.com>
2024-02-24 17:01:42 -0300

    fix: adjust class EducationLevel

--- src/main/java/com/example/cloudhuman/models/EducationLevel.java
+++ src/main/java/com/example/cloudhuman/models/EducationLevel.java
@@ -1,6 +1,5 @@
 package com.example.cloudhuman.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +11,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class EducationLevel {
 
-    @JsonProperty("no_education")
-    private boolean  noEducation;
-    @JsonProperty("high_school")
-    private boolean  highSchool;
-    @JsonProperty("bachelors_degree_or_high")
+    private boolean noEducation;
+    private boolean highSchool;
     private boolean bachelorsDegreeOrHigh;
 
     public EducationLevel(String value) {
@@ -27,7 +23,7 @@ public class EducationLevel {
         } else if (value.equals("bachelors_degree_or_high")) {
             this.bachelorsDegreeOrHigh = true;
         } else {
-            throw new IllegalArgumentException("Valor inválido para EducationLevel: " + value);
+            throw new IllegalArgumentException("Invalid value for EducationLevel: " + value);
         }
     }
 }
