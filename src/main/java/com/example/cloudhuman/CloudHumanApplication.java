0aa76ca8609a71c6bf73d79a92f6acfaff0d6106
Edvan Junior <edvanbrito.j@hotmail.com>
2024-02-25 01:13:13 -0300

    feat: added swagger doc

--- src/main/java/com/example/cloudhuman/CloudHumanApplication.java
+++ src/main/java/com/example/cloudhuman/CloudHumanApplication.java
@@ -1,9 +1,12 @@
 package com.example.cloudhuman;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Cloud Humans API", version = "1"))
 public class CloudHumanApplication {
 
 	public static void main(String[] args) {
