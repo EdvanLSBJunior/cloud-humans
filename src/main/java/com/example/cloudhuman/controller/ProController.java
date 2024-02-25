0c61db94c8887006f102902975663ee3e8220164
Edvan Junior <edvanbrito.j@hotmail.com>
2024-02-25 01:16:31 -0300

    fix: adjust validation and added swagger descriptions

--- src/main/java/com/example/cloudhuman/controller/ProController.java
+++ src/main/java/com/example/cloudhuman/controller/ProController.java
@@ -3,35 +3,37 @@ package com.example.cloudhuman.controller;
 import com.example.cloudhuman.exceptions.InvalidWritingScoreException;
 import com.example.cloudhuman.models.Pro;
 import com.example.cloudhuman.services.ProService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pro")
+@Tag(name = "pro")
 public class ProController {
 
     @Autowired
     private ProService service;
 
-    @Autowired
-    private ObjectMapper mapper;
-
+    @Operation(summary = "Verify indicate best project for pro" , method="POST")
     @PostMapping(value = "/validate")
-    public ResponseEntity<String> evaluate(@RequestBody String json) throws Exception {
-        Pro pro = mapper.readValue(json, Pro.class);
-        try {
-            float writingScore = pro.getWritingScore();
-            if (writingScore < 0 || writingScore > 1) {
-                throw new InvalidWritingScoreException("writing_score must be between 0 and 1");
-            }
+    public ResponseEntity<String> evaluate(@Valid @RequestBody Pro pro, BindingResult bindingResult) throws Exception {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldError().getDefaultMessage());
+        }
 
+        try {
             String response = service.assignProjectByScore(pro);
             return ResponseEntity.ok(response);
 
-        } catch (InvalidWritingScoreException | JsonProcessingException e) {
+        } catch (InvalidWritingScoreException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
